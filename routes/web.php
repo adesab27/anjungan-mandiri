ea47eff238bce88d78533df637bed3066c4853ea
Adelia <adeliasalsabila1001@gmail.com>
2026-04-13 20:26:46 +0700

    delete

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AntrianController;
 
 
 Route::get('/pengunjung', function () {
@@ -16,6 +15,3 @@ Route::get('/petugas', function () {
     return view('petugas');
 });
 
-Route::post('/ambil-antrian', [AntrianController::class, 'ambil']);
-
-Route::get('/antrian-terakhir', [AntrianController::class, 'terakhir']);
